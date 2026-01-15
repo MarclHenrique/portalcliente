f39a6e599363f96f1e6420b4aa04926ff91def90
MarclHenrique <mh593417@gmail.com>
2026-01-15 07:00:29 -0300

    Configuração inicial do projeto

---
+++ src/test/java/br/com/coderbank/portalCliente/PortalClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.coderbank.portalCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
